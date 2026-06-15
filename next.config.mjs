8bdc4f9840e7a7ddd2fd9f585fe3ac58fb9e93a2
fabian4819 <habibfabianfahlesi2004@mail.ugm.ac.id>
2026-06-15 19:17:25 +0700

    Redirect demo route to app

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/demo",
+        destination: "/app",
+        permanent: false,
+      },
+      {
+        source: "/demo/:path*",
+        destination: "/app/:path*",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
